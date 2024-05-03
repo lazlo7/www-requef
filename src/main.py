030d8e545adb555f173335fd274aa1e2a3825056
Max Smagin <lazlogogalac777@gmail.com>
2024-05-03 22:06:36 +0300

    Added default 404 page

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from random import choice
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, _):
+    comments = [
+        "uhhhm, you sure this what you looking for?",
+        "this might be a dead end",
+        "you're lost, aren't you?",
+        "you might want to turn back",
+        "nothing's here, nobody's here"
+    ]
+    return templates.TemplateResponse("404.html", {"request": request, "comment": choice(comments)})
 
 @app.get("/")
 async def root(request: Request):
