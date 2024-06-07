ff1eae636c1e4e322d488d114f0f9ed1b7c13abd
Max Smagin <lazlogogalac777@gmail.com>
2024-06-07 19:05:31 +0300

    Changed flakes to use poetry

--- flake.nix
+++ flake.nix
@@ -1,24 +1,31 @@
 {
-  description = "Dev shell";
-
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-
-  outputs = { self, nixpkgs, ... }:
+  description = "Development shell for catris";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = { self, nixpkgs, poetry2nix, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.x86_64-linux.default =
-        pkgs.mkShell
-          {
-            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-            shellHook = ''
-              source .venv/bin/activate
-            '';
-          };
-    };
+      pkgs = import nixpkgs { 
+        system = system;
+        config = {
+          allowUnfree = true;
+        }; 
+      };
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      wwwrequef = mkPoetryApplication { 
+        projectDir = ./.;
+        python = pkgs.python3;
+        pyproject = ./pyproject.toml;
+        poetryLock = ./poetry.lock; 
+      };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        buildInputs = with pkgs; [
+          wwwrequef
+        ];    
+      };
+  };
 }
