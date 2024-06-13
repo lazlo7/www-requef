224156e024c68046de68f6c4f325f3c2b2ab99c5
Max Smagin <lazlogogalac777@gmail.com>
2024-06-13 16:22:14 +0300

    Pinned orjson version to 3.10.3 to fix hash mismatch on build

--- flake.nix
+++ flake.nix
@@ -1,27 +1,22 @@
 {
   description = "Development shell for webserver";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
   outputs = { self, nixpkgs, poetry2nix, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { 
-        system = system;
-        config = {
-          allowUnfree = true;
-        }; 
-      };
+      pkgs = nixpkgs.legacyPackages.${system}; 
       inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       wwwrequef = mkPoetryApplication { 
         projectDir = ./.;
-        python = pkgs.python3;
+        python = pkgs.python311;
         pyproject = ./pyproject.toml;
         poetryLock = ./poetry.lock; 
       };
     in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell {
         LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
         buildInputs = with pkgs; [
           wwwrequef
